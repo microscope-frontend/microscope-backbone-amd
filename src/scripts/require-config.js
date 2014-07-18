86bad644636645df4aa32a474be84e9096f48701
Benjamin HEINTZ <benjamin.heintz@almerys.com>
2014-07-18 14:38:51 +0200

    fix r.js bugs

--- src/scripts/require-config.js
+++ src/scripts/require-config.js
@@ -1,31 +1,33 @@
 require.config({
-  baseUrl: '/scripts/',
-  paths:{
-      'jquery'    : 'vendors/jquery',
-      'bootstrap' : 'vendors/bootstrap',
-      'underscore': 'vendors/underscore',
-      'handlebars': 'vendors/handlebars',
-      'backbone'  : 'vendors/backbone',
-      'marionette'  : 'vendors/marionette',
-      'hbs'       : 'vendors/hbs',
-      'text'      : 'vendors/text'
-  },
-  shim: {
-      'jquery': {'exports' : '$'},
-      'bootstrap': ['jquery'],
-      'handlebars': {
-          exports: 'Handlebars'
-      },
-      'backbone': {
-          exports: 'Backbone',
-          deps: ['jquery', 'underscore']
-      },
-      'marionette' : {
-          deps : ['jquery', 'underscore', 'backbone'],
-          exports : 'Marionette'
-      },
-      'underscore': {
-          exports: '_'
-      }
+    baseUrl: '/scripts/',
+    paths: {
+        'jquery': 'vendors/jquery',
+        'bootstrap': 'vendors/bootstrap',
+        'underscore': 'vendors/underscore',
+        'handlebars': 'vendors/handlebars',
+        'backbone': 'vendors/backbone',
+        'marionette': 'vendors/marionette',
+        'hbs': 'vendors/hbs',
+        'text': 'vendors/text'
+    },
+    shim: {
+        'jquery': {
+            'exports': '$'
+        },
+        'bootstrap': ['jquery'],
+        'handlebars': {
+            exports: 'Handlebars'
+        },
+        'backbone': {
+            exports: 'Backbone',
+            deps: ['jquery', 'underscore']
+        },
+        'marionette': {
+            deps: ['jquery', 'underscore', 'backbone'],
+            exports: 'Marionette'
+        },
+        'underscore': {
+            exports: '_'
+        }
     }
 });
